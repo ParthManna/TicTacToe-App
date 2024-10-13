0f0a374cfe67c6568b4aabf5f02337ada67f4d86
PARTHA SARATHI MANNA <mannapartha2004@gmail.com>
2024-10-14 04:58:20 +0530

    add player two win MainActivity5.kt

---
+++ app/src/main/java/com/example/tictactoe2/MainActivity5.kt
@@ -0,0 +1,27 @@
+package com.example.tictactoe2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tictactoe2.databinding.ActivityMain5Binding
+
+class MainActivity5 : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMain5Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain5Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.backbutton.setOnClickListener {
+            val intent = Intent(this, MainActivity3::class.java)
+            startActivity(intent)
+        }
+
+        binding.playagain.setOnClickListener{
+            val intent2 = Intent(this, MainActivity::class.java)
+            startActivity(intent2)
+        }
+    }
+}
